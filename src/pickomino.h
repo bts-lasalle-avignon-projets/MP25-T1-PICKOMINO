caad67cac447a775fe49d10455ed8164bdb1e343
pmillotpro <pierre.millot.pro@gmail.com>
2024-10-16 08:21:04 +0200

    Ajouter et corriger les donnees

--- src/pickomino.h
+++ src/pickomino.h
@@ -1,35 +1,42 @@
 #ifndef PICKOMINO_H
 #define PICKOMINO_H
 
+#include <string>
+
 #define NB_JOUEURS_MIN 2
 #define NB_JOUEURS_MAX 7
 #define NB_DES 8
+#define NB_FACES 6
 #define NB_PICKOMINOS 16
 #define VALEUR_PICKOMINOS_MIN 21
 #define VALEUR_PICKOMINOS_MAX 36
 
 struct Joueur
 {
-    int nombreDeJoueurs;
-    char nomJoueur[NB_JOUEURS_MAX][7];
+    char nomJoueur[NB_JOUEURS_MAX];
     int sommeTotalDes;        
     int versTotal;
     int pilePickomino[NB_PICKOMINOS];
     int sommetPile;  
 };
 
-extern Joueur joueur;
-
-struct Pickomino {
-    int valeur;
-    int nombreVers;    
+struct Plateau
+{
+    const int brochettePickominos[NB_PICKOMINOS];
 };
 
-const Pickomino brochettePickominos[NB_PICKOMINOS] = {
-    {21, 1}, {22, 1}, {23, 1}, {24, 1},
-    {25, 2}, {26, 2}, {27, 2}, {28, 2},
-    {29, 3}, {30, 3}, {31, 3}, {32, 3},
-    {33, 4}, {34, 4}, {35, 4}, {36, 4}
+enum Etat
+{
+    VISIBLE,
+    RETOURNE,
+    PRISE
+};
+
+struct Pickomino
+{
+    int valeur;
+    int nombreVers;
+    enum Etat etat;
 };
 
 #endif

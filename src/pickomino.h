28ebc69bc86c62ce490557ab94875d52b11c7aa6
Thierry Vaira <tvaira@free.fr>
2024-10-16 08:30:57 +0200

    Remanie le code

--- src/pickomino.h
+++ src/pickomino.h
@@ -5,24 +5,20 @@
 
 #define NB_JOUEURS_MIN 2
 #define NB_JOUEURS_MAX 7
-#define NB_DES 8
+
+#define NB_DES   8
 #define NB_FACES 6
-#define NB_PICKOMINOS 16
+
+#define NB_PICKOMINOS         16
 #define VALEUR_PICKOMINOS_MIN 21
 #define VALEUR_PICKOMINOS_MAX 36
 
 struct Joueur
 {
-    char nomJoueur[NB_JOUEURS_MAX];
-    int sommeTotalDes;        
-    int versTotal;
-    int pilePickomino[NB_PICKOMINOS];
-    int sommetPile;  
-};
-
-struct Plateau
-{
-    const int brochettePickominos[NB_PICKOMINOS];
+    std::string nom;
+    int         versTotal;
+    int         pilePickomino[NB_PICKOMINOS];
+    int         sommetPile;
 };
 
 enum Etat
@@ -34,9 +30,16 @@ enum Etat
 
 struct Pickomino
 {
-    int valeur;
-    int nombreVers;
-    enum Etat etat;
+    int  valeur;
+    int  nombreVers;
+    Etat etat;
+};
+
+struct Plateau
+{
+    Pickomino brochettePickominos[NB_PICKOMINOS];
+    int       sommeTotalDes;
+    int       des[NB_DES];
 };
 
 #endif

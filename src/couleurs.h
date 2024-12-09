976a1bbcd0e340864ce209496bc3fb252e3598d0
pmillotpro <pierre.millot.pro@gmail.com>
2024-12-09 12:09:29 +0100

    Ajouter couleurs

---
+++ src/couleurs.h
@@ -0,0 +1,15 @@
+#ifndef COULEURS_H
+#define COULEUR_H
+
+#define COULEUR_DEFAUT "\033[0m"
+#define BLEU           "\033[34m"
+#define VERT           "\033[32m"
+#define ROUGE          "\033[31m"
+#define JAUNE          "\033[33m"
+
+#define BLEU_GRAS  "\033[1m\033[34m"
+#define VERT_GRAS  "\033[1m\033[32m"
+#define ROUGE_GRAS "\033[1m\033[31m"
+#define JAUNE_GRAS "\033[1m\033[33m"
+
+#endif

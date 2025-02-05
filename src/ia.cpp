12323c625ce7ab7ddce3bbc2587b539702378d78
Votre Nom <matteo.navarro.pro@gmail.com>
2025-02-05 23:48:18 +0100

    Jouer une partie contre un ordinateur moyen

--- src/ia.cpp
+++ src/ia.cpp
@@ -5,6 +5,7 @@
 #include <ctime>
 #include <thread>
 #include <chrono>
+#include <iostream>
 
 void initialiserPartieIA(Jeu& jeu)
 {
@@ -134,54 +135,61 @@ int choisirFaceAleatoire(Plateau& plateau)
 
 int choisirFacePlusGrandTotal(Plateau& plateau)
 {
-    int faceOccurence[NB_FACES];
-    int valeurTotalFace[NB_FACES];
-    int faceOccurencePlusEleve = -1;
+    int faceOccurence[NB_FACES] = {0,0,0,0,0,0};
+    int valeurTotalFace[NB_FACES] = {0,0,0,0,0,0};
+    int faceOccurencePlusEleve = 0;
 
-    if(plateau.desRetenus[FACE_VER - 1] > 0 || presenceVerDansLancer(plateau))
+    if(plateau.desRetenus[FACE_VER - 1] > 0 || !presenceVerDansLancer(plateau))
     {
         for(int i = 0; i < plateau.nbDes; ++i)
         {
             if(plateau.desRetenus[plateau.des[i] - 1] == 0)
             {
-                faceOccurence[plateau.des[i]]++;
+                faceOccurence[plateau.des[i] - 1]++;
             }
             else
             {
-                faceOccurence[plateau.des[i]] = 0;
+                faceOccurence[plateau.des[i] - 1] = 0;
             }
         }
 
         for(int i = 0; i < NB_FACES; ++i)
         {
-            valeurTotalFace[i] = faceOccurence[i] * i;
+            valeurTotalFace[i] = faceOccurence[i] * (i + 1);
         }
 
         for(int i = 0; i < NB_FACES; ++i)
         {
             if(valeurTotalFace[i] == valeurTotalFace[faceOccurencePlusEleve])
             {
-                if(i < faceOccurencePlusEleve)
+                if(i > faceOccurencePlusEleve)
                 {
                     faceOccurencePlusEleve = i;
                 }
             }
-            else if(valeurTotalFace[i] > valeurTotalFace[faceOccurencePlusEleve])
+            else if(faceOccurencePlusEleve != 0)
+            {
+                if(valeurTotalFace[i] > valeurTotalFace[faceOccurencePlusEleve])
+                {
+                    faceOccurencePlusEleve = i;
+                }
+            }
+            else
             {
                 faceOccurencePlusEleve = i;
             }
         }
-        return faceOccurencePlusEleve;
+        return faceOccurencePlusEleve + 1;
     }
-
     return FACE_VER;
+
 }
 
 bool presenceVerDansLancer(Plateau& plateau)
 {
     for(int i = 0; i < plateau.nbDes; ++i)
     {
-        if(plateau.des[i] == FACE_VER - 1)
+        if(plateau.des[i] == FACE_VER)
         {
             return true;
         }
